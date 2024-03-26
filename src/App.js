bb545f6a5806abf727e361c1b7a622ba35bb1405
abdelilah <abdelilahkouzih2003@gmail.com>
2024-03-26 16:38:54 +0000

    update 11

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and savehhhhkkkkkkkkkkkhhhhhhhhhhhh to reload.
+          Edit <code>src/App.js</code> salam alawi habib galbiiii to reload.
         </p>
         <a
           className="App-link"
